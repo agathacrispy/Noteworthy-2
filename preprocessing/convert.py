6d22aaafe74f0d7cc26c01d28d14a7cf1d76079f
agathacrispy <ericrao08@gmail.com>
2026-06-18 19:50:04 -0400

    solidified preprocessing pipeline

--- preprocessing/convert.py
+++ preprocessing/convert.py
@@ -5,7 +5,6 @@ from pydub import AudioSegment
 def mp3_to_wav(mp3_path: Path) -> Path:
     
     #convert mp3 to wav, returns path to new wav file
-    #mp3 file MAY be deleted later
     
     wav_path = mp3_path.with_suffix(".wav")
 
@@ -15,3 +14,14 @@ def mp3_to_wav(mp3_path: Path) -> Path:
     mp3_path.unlink()
 
     return wav_path
+
+def wav_to_mp3(wav_path: Path) -> Path:
+    
+    mp3_path = wav_path.with_suffix(".mp3")
+
+    audio = AudioSegment.from_mp3(str(wav_path))
+    audio.export(str(mp3_path), format="wav")
+    
+    wav_path.unlink()
+
+    return mp3_path
